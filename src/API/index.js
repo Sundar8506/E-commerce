0f28c23bed5bd0a6c29d7c83b7512a3f7d36a249
Sundhareshan <sundhareshan8506@mountzion.ac.in>
2025-07-11 22:15:45 +0530

    new

---
+++ src/API/index.js
@@ -0,0 +1,48 @@
+
+
+export const getAllProducts = () => {
+  return fetch("https://dummyjson.com/products").then((res) => res.json());
+};
+export const getProductsByCategory = (category) => {
+  return fetch(`https://dummyjson.com/products/category/${category}`).then(
+    (res) => res.json()
+  );
+};
+export const getCart = (cartProductIds) => {
+  
+
+  return fetch("https://dummyjson.com/carts/add", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      userId: 1,
+      products: [
+        {
+          id:5,
+          
+
+               
+        
+        },
+        
+        console.log(cartProductIds)
+      ],
+    }),
+  }).then((res) => res.json());
+};
+
+export const addToCart = (id) => {
+  return fetch("https://dummyjson.com/carts/add", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      userId: 1,
+      products: [
+        {
+          id: id,
+          quantity:4,
+        },
+      ],
+    }),
+  }).then((res) => res.json());
+};
